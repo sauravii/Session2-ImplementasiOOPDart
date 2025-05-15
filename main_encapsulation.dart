7ff54ee36693e76fe02f9ea5dd5f6dbb9944046a
Sheren Aura Vi <sherenaura21@gmail.com>
2025-05-16 06:19:18 +0700

    encapsulation

---
+++ main_encapsulation.dart
@@ -0,0 +1,33 @@
+// Encapsulation
+class Mobil {
+  String? _merk = '';
+  int _kecepatan = 0;
+
+  Mobil(this._merk, this._kecepatan);
+
+  // getter untuk merk
+  String? getMerk() {
+    print("Merknya adalah $_merk");
+    return _merk;
+  }
+
+  // setter untuk merk
+  void setMerk(String value) {
+    if (value.isEmpty) {
+      print('Merk tidak boleh kosong');
+    } else {
+      _merk = value;
+      print("Merk menjadi $_merk");
+    }
+  }
+}
+
+void main() {
+  Mobil Avanza = Mobil("Palisade", 150);
+
+  // getter
+  Avanza.getMerk();
+
+  // setter
+  Avanza.setMerk("Toyota");
+}
